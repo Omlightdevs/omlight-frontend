69eee9056a3d94f03336836758d1acfbd7d189a6
aakashmistry-ansorbit <aakash.mistry@ansorbit.com>
2021-08-08 18:30:55 +0530

    bug fixed

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,12 @@
 const Config = {
-     get PUBLIC_URL() {
-          return process.env.PUBLIC_URL;
-     },
-     get SERVER_URL() {
-          return `http://localhost:8080/api/1.0`;
-     },
-     get PRODUCTION_URL() {
-          return `https://om-lite-admin.herokuapp.com/`
-     }
+  get PUBLIC_URL() {
+    return process.env.PUBLIC_URL;
+  },
+  get SERVER_URL() {
+    return `https://om-lite-admin.herokuapp.com/api/1.0`;
+  },
+  get PRODUCTION_URL() {
+    return `https://om-lite-admin.herokuapp.com/`;
+  },
 };
 export default Config;
